f565aa8616fb87d5fcb4bf14927b498d60d4dedc
Abrham <65964297+Abrssh@users.noreply.github.com>
2025-03-21 23:06:50 +0300

    Adds a fix to the Scroll controller error - Solves the "The provided ScrollController is attached to more than one ScrollPosition." error using a Cubit to store the scroll offsets and index while also ditching the TabBarView

--- lib/Presentation/Pages/ui_training/nested_scrollview_tab_bar_page.dart
+++ lib/Presentation/Pages/ui_training/nested_scrollview_tab_bar_page.dart
@@ -1,10 +1,12 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+/*
+Solves the "The provided ScrollController is attached to more than one ScrollPosition." error using a Cubit to store the scroll offsets and index while also ditching the TabBarView because the error is most probably caused by the TabBarView trying to store the TabBar Contents in cache so the scroll controller becomes attached to multiple scroll positions
+ */
 void main() => runApp(const MyApp());
 
-// This code causes primaryscroll controller is attached to more than one scrollposition error but it works as expected
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,103 +15,193 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fixed AppBar Scroll Demo',
       theme: ThemeData(useMaterial3: true),
-      home: const HomeScreen(),
+      home: const NestedScrollViewTabBarPage(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+// Cubit to store scroll offsets
+class ValScroll {
+  final int activeIndex;
+  final List<double> offsets;
+  ValScroll(this.activeIndex, this.offsets);
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  copywith({int? activeIndex, List<double>? offsets}) {
+    return ValScroll(activeIndex ?? this.activeIndex, offsets ?? this.offsets);
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+class TabOffsetCubit extends Cubit<ValScroll> {
+  TabOffsetCubit() : super(ValScroll(0, [0, 0, 0]));
+
+  void setActiveIndex(int index) {
+    emit(state.copywith(activeIndex: index));
+  }
+
+  void saveOffset(int updateIndex, double offset) {
+    List<double> newOffsets = [];
+    newOffsets.addAll(state.offsets);
+    for (int i = 0; i < newOffsets.length; i++) {
+      if (i == updateIndex) {
+        newOffsets[i] = offset;
+        break;
+      }
+    }
+    emit(state.copywith(offsets: newOffsets));
+  }
+
+  int getActiveIndex(int index) {
+    return state.activeIndex;
+  }
+}
+
+class NestedScrollViewTabBarPage extends StatefulWidget {
+  const NestedScrollViewTabBarPage({super.key});
+
+  @override
+  State<NestedScrollViewTabBarPage> createState() =>
+      _NestedScrollViewTabBarPageState();
+}
+
+class _NestedScrollViewTabBarPageState extends State<NestedScrollViewTabBarPage>
+    with TickerProviderStateMixin {
   late final TabController _tabController;
-  final List<PageStorageKey> _tabKeys =
-      List.generate(3, (i) => PageStorageKey('tab_$i'));
+  final TabOffsetCubit _offsetCubit = TabOffsetCubit();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      _offsetCubit.setActiveIndex(_tabController.index);
+      setState(() {});
+    });
   }
 
   @override
   void dispose() {
     _tabController.dispose();
+    _offsetCubit.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NestedScrollView(
-        // No custom controller here; let NestedScrollView manage the primary controller.
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverOverlapAbsorber(
-            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-            sliver: SliverAppBar(
-              title: const Text('Collapsible AppBar Demo'),
-              pinned: false, // Allow full collapse
-              floating: true, // Show on upward scroll
-              snap: true, // Snap animation
-              expandedHeight: 200,
-              bottom: TabBar(
-                controller: _tabController,
-                tabs: const [
-                  Tab(text: 'Tab 1'),
-                  Tab(text: 'Tab 2'),
-                  Tab(text: 'Tab 3'),
-                ],
-              ),
-              flexibleSpace: FlexibleSpaceBar(
-                background: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Color(0xFF3366FF), Color(0xFF00CCFF)],
+    void scrollToPosition(double position, BuildContext cxt) {
+      final primaryScrollController = PrimaryScrollController.of(cxt);
+      if (primaryScrollController.hasClients) {
+        primaryScrollController.animateTo(position,
+            duration: const Duration(
+              milliseconds: 100,
+            ),
+            curve: Curves.bounceInOut);
+      } else {
+        debugPrint("Scroll p");
+      }
+    }
+
+    return BlocProvider(
+      create: (_) => _offsetCubit,
+      child: Scaffold(
+        body: NestedScrollView(
+          // This is critical for floating behavior
+          floatHeaderSlivers: true,
+          controller: _scrollController,
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverAppBar(
+                  title: const Text('Collapsible AppBar Demo'),
+                  floating: true,
+                  snap: true,
+                  pinned: true,
+                  expandedHeight: 200,
+                  forceElevated: innerBoxIsScrolled,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF3366FF), Color(0xFF00CCFF)],
+                        ),
+                      ),
                     ),
+                  ),
+                  bottom: TabBar(
+                    controller: _tabController,
+                    tabs: const [
+                      Tab(text: 'Tab 1'),
+                      Tab(text: 'Tab 2'),
+                      Tab(text: 'Tab 3'),
+                    ],
                   ),
                 ),
               ),
-            ),
+            ];
+          },
+
+          body: BlocConsumer<TabOffsetCubit, ValScroll>(
+            listener: (context, state) {
+              debugPrint("Offset Rm: ${state.offsets[state.activeIndex]}");
+
+              scrollToPosition(state.offsets[state.activeIndex], context);
+            },
+            listenWhen: (previous, current) =>
+                previous.activeIndex != current.activeIndex,
+            builder: (context, state) {
+              return _TabContent(index: state.activeIndex);
+            },
+            buildWhen: (previous, current) =>
+                previous.activeIndex != current.activeIndex,
           ),
-        ],
-        // Do not wrap TabBarView with a global Scrollbar.
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            _buildTabContent(0),
-            _buildTabContent(1),
-            _buildTabContent(2),
-          ],
         ),
       ),
     );
   }
+}
 
-  Widget _buildTabContent(int index) {
+class _TabContent extends StatelessWidget {
+  final int index;
+
+  const _TabContent({required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the cubit
+    final offsetCubit = context.watch<TabOffsetCubit>();
+    debugPrint(
+        "Active Index: ${offsetCubit.getActiveIndex(0)} ${offsetCubit.state.offsets}");
+
+    // Use Builder to get the correct context for NestedScrollView
     return Builder(
-      builder: (context) {
-        // Wrap each CustomScrollView with its own Scrollbar.
-        return Scrollbar(
+      builder: (BuildContext context) {
+        return NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification notification) {
+            if (notification is ScrollUpdateNotification) {
+              // debugPrint("Notification: ${notification.metrics.pixels}");
+              offsetCubit.saveOffset(index, notification.metrics.pixels);
+            }
+            return false;
+          },
           child: CustomScrollView(
-            key: _tabKeys[index],
-            // No custom controller; use the NestedScrollView's primary controller.
-            slivers: [
+            primary: offsetCubit.getActiveIndex(0) == index,
+            slivers: <Widget>[
               SliverOverlapInjector(
                 handle:
                     NestedScrollView.sliverOverlapAbsorberHandleFor(context),
               ),
               SliverPadding(
-                padding: const EdgeInsets.all(16),
+                padding: const EdgeInsets.all(8.0),
                 sliver: SliverList(
                   delegate: SliverChildBuilderDelegate(
-                    (context, i) => ListTile(
-                      title: Text('Item ${i + 1}'),
-                      subtitle: Text('Tab ${index + 1} Content'),
-                    ),
+                    (BuildContext context, int i) {
+                      return ListTile(
+                        title: Text('Item ${i + 1}'),
+                        subtitle: Text('Tab ${index + 1} Content'),
+                      );
+                    },
                     childCount: 100,
                   ),
                 ),
@@ -122,7 +214,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   }
 }
 
-// Creating half circles using custom painter
+// Keep the RiskRatingCircle and HalfCirclePainter classes as they were
 class RiskRatingCircle extends StatelessWidget {
   const RiskRatingCircle({super.key});
 
@@ -195,14 +287,6 @@ class HalfCirclePainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = strokeWidth
       ..strokeCap = StrokeCap.round;
-
-    // canvas.drawArc(
-    //   Rect.fromCircle(center: center, radius: radius),
-    //   pi / 2, // Start at bottom
-    //   pi, // 180 degrees
-    //   false,
-    //   orangePaint,
-    // );
 
     canvas.drawArc(
       Rect.fromCircle(center: const Offset(130, 100), radius: radius2),
